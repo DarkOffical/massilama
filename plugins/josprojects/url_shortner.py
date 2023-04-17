d56db8108102df7094913d5339006a71aa77a27d
DarkOffical <123810865+DarkOffical@users.noreply.github.com>
2023-04-17 14:15:33 +0530

    Update url_shortner.py

--- plugins/josprojects/url_shortner.py
+++ plugins/josprojects/url_shortner.py
@@ -27,7 +27,7 @@ GPLINKS_API = os.environ.get("GPLINKS_API", "008ccaedd6061ad1948838f410947603de9
 
 reply_markup = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton(text='join projects channel', url='https://t.me/movieclub1241')
+        InlineKeyboardButton(text='join projects channel', url='https://t.me/+TbWfBUt0ZStmMmE9')
         ]]
     )
 
@@ -190,7 +190,7 @@ async def short(link):
     
     # Send the text
     try:
-        shorten_urls += "\n\ʎᗺ ǝpɐꟽ: @movieclub1241 🔥"
+        shorten_urls += "\n\ʎᗺ ǝpɐꟽ: @film_cube_group 🔥"
         return shorten_urls
     except Exception as error:
         return error
