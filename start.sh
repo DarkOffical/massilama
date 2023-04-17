2b37b77174544870723b3bf1eea20657de15df71
DarkOffical <123810865+DarkOffical@users.noreply.github.com>
2023-04-17 13:19:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/darkpc1412/massilama.git /massilama
+git clone -b master https://github.com/DarkOffical/massilama.git /massilama
 cd /massilama
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
